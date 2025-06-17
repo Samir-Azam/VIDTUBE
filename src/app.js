b4a0c8fa13a3fc4f55b31144fe95975d2c309420
Samir Azam <azamsamir190@gmail.com>
2025-06-17 13:23:37 +0530

    create healthcheck routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,8 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-export { app };
+
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+export {app}
